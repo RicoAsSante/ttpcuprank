f8be3a00be3bf6f3985e4ea4ccb6d6ad96e35dab
RicoAsSante <38320940+RicoAsSante@users.noreply.github.com>
2018-04-12 16:47:28 +0300

    Update

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,13 @@
-var cs = { 
-  "apiKey": 'AIzaSyAqEbjXkXabxqpxYah2KWjOG3rnE6DuNZ0', 
-  "authDomain": 'ttp-cup-rating.firebaseapp.com', 
-  "databaseURL": 'https://ttp-cup-rating.firebaseio.com' 
-};
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAqEbjXkXabxqpxYah2KWjOG3rnE6DuNZ0",
+    authDomain: "ttp-cup-rating.firebaseapp.com",
+    databaseURL: "https://ttp-cup-rating.firebaseio.com",
+    projectId: "ttp-cup-rating",
+    storageBucket: "ttp-cup-rating.appspot.com",
+    messagingSenderId: "1094925165897"
+  };
+  firebase.initializeApp(config);
+</script>
