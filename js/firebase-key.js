a9a95aeb70d8d36eba37a58e578c326a3a3cdc50
RicoAsSante <38320940+RicoAsSante@users.noreply.github.com>
2018-04-12 16:51:08 +0300

    Add files via upload

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
-  var config = {
-    apiKey: "AIzaSyAqEbjXkXabxqpxYah2KWjOG3rnE6DuNZ0",
-    authDomain: "ttp-cup-rating.firebaseapp.com",
-    databaseURL: "https://ttp-cup-rating.firebaseio.com",
-  };
+var cs = { 
+  "apiKey": 'AIzaSyAqEbjXkXabxqpxYah2KWjOG3rnE6DuNZ0', 
+  "authDomain": 'ttp-cup-rating.firebaseapp.com', 
+  "databaseURL": 'https://ttp-cup-rating.firebaseio.com' 
+};
