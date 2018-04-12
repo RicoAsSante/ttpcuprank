ba20dc840aae383cb6729ac0d3fadb5294bf9511
RicoAsSante <38320940+RicoAsSante@users.noreply.github.com>
2018-04-12 16:16:35 +0300

    ttp-cup-rankings-app

---
+++ js/firebase-key.js
@@ -0,0 +1,5 @@
+var cs = { 
+  "apiKey": 'AIzaSyAqEbjXkXabxqpxYah2KWjOG3rnE6DuNZ0', 
+  "authDomain": 'ttp-cup-rating.firebaseapp.com', 
+  "databaseURL": 'https://ttp-cup-rating.firebaseio.com' 
+};
